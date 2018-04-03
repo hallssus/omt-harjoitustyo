1fd6401e60c2feb47697c5958b939b1d7430cce4
hallssus <susanna.hallsten@helsinki.fi>
2018-04-03 15:07:09 +0300

    Snake

---
+++ Snake/src/main/java/parts/Direction.java
@@ -0,0 +1,9 @@
+
+package parts;
+
+
+public enum Direction {
+    
+    UP, DOWN, LEFT, RIGHT;
+    
+}
---
+++ Snake/src/main/java/parts/Worm.java
@@ -0,0 +1,20 @@
+
+package parts;
+
+import java.util.ArrayList;
+
+public class Worm {
+    
+    private ArrayList<Piece> palat;
+    private int paanX; //pään paikka
+    private int paanY;
+    private int pituus;
+    private Direction suunta;
+
+    public Worm() {
+    
+    }
+    
+    
+    
+}
