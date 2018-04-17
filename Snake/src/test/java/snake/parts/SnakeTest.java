dc19bcd5f6cafccf7f237c57a93ce21a404e92c3
hallssus <susanna.hallsten@helsinki.fi>
2018-04-17 23:27:20 +0300

    checkstyle ja pom kuntoon

--- Snake/src/test/java/snake/parts/SnakeTest.java
+++ Snake/src/test/java/snake/parts/SnakeTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package parts;
+package snake.parts;
 
 import snake.parts.Snake;
 import org.junit.After;
